6164944a58f79cc565956c84e2ccd16e287b517c
Ayko Schwedler <ayko.schwedler@uni-oldenburg.de>
2023-10-22 16:43:01 +0200

    now shows when a reCAPTCHA blocks the website, code cleanup

--- src/listing_getter.py
+++ src/listing_getter.py
@@ -1,7 +1,16 @@
 from bs4 import BeautifulSoup
 from playwright.sync_api import sync_playwright
 import re
+import logging
 import pprint
+
+logging.basicConfig(
+    format="[%(asctime)s | %(levelname)s] - %(message)s ",
+    level=logging.INFO,
+    datefmt="%Y-%m-%d_%H:%M:%S",
+    handlers=[logging.FileHandler("../debug.log"), logging.StreamHandler()],
+)
+logger = logging.getLogger("bot")
 
 
 class ListingGetter:
@@ -9,10 +18,21 @@ class ListingGetter:
         with sync_playwright() as p:
             browser = p.chromium.launch(headless=True)
             page = browser.new_page()
-            page.goto(url, timeout=0) # millisecond timeout
-            html = page.inner_html("#main_column")   
-        
+            page.goto(url, timeout=0)  # millisecond timeout
+            html = page.inner_html("#main_column")
+
         soup = BeautifulSoup(html, "lxml")
+
+        # Find all <p> tags on the page
+        p_tags = soup.find_all("p")
+
+        # Check for the word "Captchas" in <p> tags and log when found
+        for p_tag in p_tags:
+            if "Captchas" in p_tag.get_text():
+                # Log the occurrence of "Captchas" within <p> tags
+                logger.critical("Captcha detected. Retrying.")
+                break
+
         # get all listing elements by looking for 'liste-details-ad-#####'
         self.listings = soup.find_all("div", id=re.compile("^liste-details-ad-\d+"))
 
@@ -31,16 +51,15 @@ class ListingGetter:
             raise ValueError("Not all lists have the same length!")
 
         # write dict for all listings
-        for i, (ref, user_name, address, wg_type, rental_length_months) in enumerate(zip(refs, user_names, addresses, wg_types, rental_lengths_months)):
-            
+        for i, (ref, user_name, address, wg_type, rental_length_months) in enumerate(
+                zip(refs, user_names, addresses, wg_types, rental_lengths_months)):
+
             # skip promotes offers from letting agencies
             if "\n" in user_name:
                 continue
 
-            listing_dict = {}
-            listing_dict["ref"], listing_dict["user_name"] = ref, user_name
-            listing_dict["address"], listing_dict["wg_type"] = address, wg_type
-            listing_dict["rental_length_months"] = rental_length_months
+            listing_dict = {"ref": ref, "user_name": user_name, "address": address, "wg_type": wg_type,
+                            "rental_length_months": rental_length_months}
             info_dict[i] = listing_dict
         return info_dict
 
@@ -93,7 +112,7 @@ class ListingGetter:
 
         # get time difference in months
         date_diff = (int(end_year) - int(start_year)) * 12 + (
-            int(end_month) - int(start_month)
+                int(end_month) - int(start_month)
         )
         return date_diff
 
